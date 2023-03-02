5e3a2270aeb940690d93689fa4a5898bca73dda0
Maximo-Rulli <75349174+Maximo-Rulli@users.noreply.github.com>
2023-03-02 06:18:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import sympy as sym
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Usando Sympy"
+x, y, z = sym.symbols('x y z')
 
+des = 'd'
+func = 'cos(x**2)'
+var = x
+
+def differ(f):
+    f = sym.simplify(f)
+    differ = sym.diff(f, var)
+    return str(differ)
+
+def inte(f):
+    f = sym.simplify(f)
+    inte = sym.integrate(f, var)
+    return str(inte)
+
+@app.route('/')
+def calculate():
+    if des=='d':
+        return differ(func)
+    else:
+        return inte(func)
